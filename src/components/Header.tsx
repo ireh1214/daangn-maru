2f99bab60207b32d8caa2c91be844661f0225ea2
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-08-06 18:24:46 +0900

    240806 중고거래 업데이트 - 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <header>
-<nav>
-  <h1 className='logo'>
-    logo
-</h1>
-<ul>
-  <li><Link to ="/fleamarket">중고거래 </Link></li>
-  <li><Link to ="/nearby-stores">동네업체 </Link></li>
-  <li><Link to ="/jobs">알바 </Link></li>
-  <li><Link to ="/realty">부동산 </Link></li>
-  <li><Link to ="/car">중고차 직거래 </Link></li>
-</ul>
-</nav>
+	return (
+		<header>
+			<nav>
+				<h1 className="logo">logo</h1>
+				<ul>
+					<li>
+						<Link to="/fleamarket">중고거래 </Link>
+					</li>
+					<li>
+						<Link to="/nearbyStores">동네업체 </Link>
+					</li>
+					<li>
+						<Link to="/jobs">알바 </Link>
+					</li>
+					<li>
+						<Link to="/realty">부동산 </Link>
+					</li>
+					<li>
+						<Link to="/car">중고차 직거래 </Link>
+					</li>
+				</ul>
+			</nav>
 
-
-<div className='right'>
-  <input type="text" placeholder='물품이나 동네를 검색해보세요' />
-<button type="button">채팅하기</button>
-</div>
-
-  </header>
-  ;
+			<div className="right">
+				<input type="text" placeholder="물품이나 동네를 검색해보세요" />
+				<button type="button">채팅하기</button>
+			</div>
+		</header>
+	);
 }
