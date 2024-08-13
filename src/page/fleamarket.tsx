a34f599d9b71a3c4be294b0fbca334f80a4db0f3
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-08-13 21:34:33 +0900

    240813 파비콘변경, topsec변경

--- src/page/fleamarket.tsx
+++ src/page/fleamarket.tsx
@@ -5,16 +5,18 @@ export default function Fleamarket() {
 	return (
 		<Layout>
 			<section className="top_sec fleamarket">
-				<h2>
-					믿을만한
-					<br />
-					이웃 간 중고거래
-				</h2>
-				<p>
-					동네 주민들과 가깝고 따뜻한 거래를
-					<br />
-					지금 경험해보세요.
-				</p>
+				<div>
+					<h2>
+						믿을만한
+						<br />
+						이웃 간 중고거래
+					</h2>
+					<p>
+						동네 주민들과 가깝고 따뜻한 거래를
+						<br />
+						지금 경험해보세요.
+					</p>
+				</div>
 			</section>
 			<section className="main_sec fleamarket">
 				<h2>중고거래 인기매물</h2>
